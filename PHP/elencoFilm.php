cefb384b43488d390b0fa65990afef04a3854be5
chirivilorenzo <chirivilorenzo@gmail.com>
2024-01-26 09:52:34 +0100

    aggiunta pagina elencoFilmhtml che richiama server

--- PHP/elencoFilm.php
+++ PHP/elencoFilm.php
@@ -1,39 +1,23 @@
 <?php
-    if(!isset($_SESSION)){
-        session_start();
-    }
 
-    if(!isset($_SESSION["username"])){
-        header("Location: ../HTML/login.html");
-    }
+header('Content-Type: application/json');
 
-    //collegarsi al db per prendere i film
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "prenotazionecinema";
+// Sostituisci i segnaposto con le tue informazioni sul database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "prenotazionecinema";
+
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+$mysqli->set_charset('utf8mb4');
 
 
-    $mysqli = new mysqli($servername, $username, $password, $dbname);
-    $mysqli->set_charset('utf8mb4');
+$result = $mysqli->query("SELECT * FROM film");
 
-    $result = $mysqli->query("SELECT * FROM film");
-?>
-<html>
-    <head>
-        <link rel="stylesheet" href="../CSS/style_elencoFilm.css">
-    </head>
-    <body>
-        <table>
-            <?php
-                while(($row = $result->fetch_assoc()) != null){
-                    echo "<tr>";
-                    echo "<td><a href='schedaFilm.php?id=".$row["ID"] . "' target='_blank'>
-                    <img src='data:image/png;base64,". base64_encode($row["locandina"]) . "'/>
-                    <br>" . $row["titolo"] . "</td>";
-                    echo "</tr>";
-                }
-            ?>
-        </table>
-    </body>
-</html>
+$film = array();
+while(($row = $result->fetch_assoc()) != null){
+    $row["locandina"] = base64_encode($row['locandina']);
+    $film[] = $row;
+}
+
+echo json_encode($film);
