c87b8c603dd91025a292a53f70baf101bdb6fd9c
chirivilorenzo <chirivilorenzo@gmail.com>
2024-02-10 10:36:44 +0100

    nascosto info email

--- PHP/classi/CMail.php
+++ PHP/classi/CMail.php
@@ -14,9 +14,9 @@
         public $contenuto;
 
         public function __construct(){
-            $this->username = "chirivilorenzo@gmail.com";
-            $this->password = "yddplfpynkpwimnb";
-            $this->mittente = "chirivilorenzo@gmail.com";
+            $this->username = "xxxxxxxxxxxx";
+            $this->password = "xxxxxxxxxxxx";
+            $this->mittente = "xxxxxxxxxxxx";
             $this->host = "smtp.gmail.com";
         }
 
